5bb1fa0e82587327143c67d80dfd4049c0b91f84
Timur Malikin <timur.malikin@gmail.com>
2016-05-27 08:31:08 +0300

    Refactor Convertor.php

--- src/Converter.php
+++ src/Converter.php
@@ -2,34 +2,30 @@
 
 namespace Converter;
 
-use function Json\jsonToArr;
-use function Json\arrToJson;
-use function Yaml\yamlToArr;
-use function Yaml\arrToYaml;
-use function Ini\iniToArr;
-use function Ini\arrToIni;
+use function Json\jsonDecode;
+use function Json\jsonEncode;
+use function Yaml\yamlDecode;
+use function Yaml\yamlEncode;
+use function Ini\iniDecode;
+use function Ini\iniEncode;
 
 function startConvert($file, $formatOutput)
 {
     $content = file_get_contents($file);
     $formatOutput = strtolower($formatOutput);
 
-    $pattern = '/\/{0,1}(?P<fileName>[a-z0-9\s]+).(?P<formatInput>\w+)$/i';
-    $matches = [];
-    preg_match($pattern, $file, $matches);
+    $fileExt = getFileExt($file);
+    $fileName = getFileName($file);
 
-    $formatInput = $matches['formatInput'];
-    $fileName = $matches['fileName'];
-
-    switch ($formatInput) {
+    switch ($fileExt) {
         case 'json':
-            $arr = jsonDecode($content);
+            $fileDecoded = jsonDecode($content);
             break;
         case 'yml':
-            $arr = yamlDecode($content);
+            $fileDecoded = yamlDecode($content);
             break;
         case 'ini':
-            $arr = iniDecode($content);
+            $fileDecoded = iniDecode($content);
             break;
         default:
             echo('Unknown input format');
@@ -38,18 +34,42 @@ function startConvert($file, $formatOutput)
 
     switch ($formatOutput) {
         case 'json':
-            $result = jsonEncode($arr);
+            $string = jsonEncode($fileDecoded);
             break;
         case 'yml':
-            $result = yamlEncode($arr);
+            $string = yamlEncode($fileDecoded);
             break;
         case 'ini':
-            $result = iniEncode($arr);
+            $string = iniEncode($fileDecoded);
             break;
         default:
             echo('Unknown output format');
             return 1;
     }
 
-    file_put_contents(__DIR__ . "/../OUT_$fileName.$formatOutput", $result);
+    fileWrite($string, $fileName, $formatOutput);
+}
+
+function fileWrite($string, $fileName, $formatOutput)
+{
+    file_put_contents(__DIR__ . "/../OUT_$fileName.$formatOutput", $string);
+}
+
+function getFileName($file)
+{
+    return getFileNameAndExt($file)['fileName'];
+}
+
+function getFileExt($file)
+{
+    return getFileNameAndExt($file)['fileExt'];
+}
+
+function getFileNameAndExt($file)
+{
+    $pattern = '/\/{0,1}(?P<fileName>[a-z0-9\s]+).(?P<fileExt>\w+)$/i';
+    $matches = [];
+    preg_match($pattern, $file, $matches);
+
+    return $matches;
 }
