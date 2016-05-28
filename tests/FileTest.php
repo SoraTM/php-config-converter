e269faffdd3a1128021f5e4567f670d8b6d56895
Timur Malikin <timur.malikin@gmail.com>
2016-05-28 18:47:11 +0300

    Added FileTest

---
+++ tests/FileTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace File\Tests;
+
+use function File\getFileName;
+use function File\getFileFormat;
+
+class FileTest extends  \PHPUnit_Framework_TestCase{
+    public function testFile(){
+        $file = "file.ext";
+
+        $this->assertEquals("file", getFileName($file));
+        $this->assertEquals("ext", getFileFormat($file));
+    }
+}
