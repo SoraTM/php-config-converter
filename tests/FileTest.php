328d11b54d9d2e63cbe2925001a329533cb04fd4
Timur Malikin <timur.malikin@gmail.com>
2016-05-28 19:03:37 +0300

    Fix File.test psr2 issue

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -5,8 +5,10 @@ namespace File\Tests;
 use function File\getFileName;
 use function File\getFileFormat;
 
-class FileTest extends  \PHPUnit_Framework_TestCase{
-    public function testFile(){
+class FileTest extends  \PHPUnit_Framework_TestCase
+{
+    public function testFile()
+    {
         $file = "file.ext";
 
         $this->assertEquals("file", getFileName($file));
