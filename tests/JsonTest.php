0e13792410a57d28e4f1fdc8874d7179110017ca
Timur Malikin <timur.malikin@gmail.com>
2016-05-28 21:35:42 +0300

    Formating code in JsonTest.php

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -12,14 +12,18 @@ class JsonTest extends \PHPUnit_Framework_TestCase
     {
         $file = file_get_contents(__DIR__ . '/inputFiles/example.json');
 
-        $arr = [ "name" => "soratm/php-config-converter",
-        "description" => "Tools for converting config files one to another (xml, yml, json)",
-        "license" => "MIT",
-        "authors" => [
-            [ "name" => "Timur Malikin",
-            "email" => "timur.malikin@gmail.com",
-            "role" => "Developer" ]
-        ]];
+        $arr = [
+            "name" => "soratm/php-config-converter",
+            "description" => "Tools for converting config files one to another (xml, yml, json)",
+            "license" => "MIT",
+            "authors" => [
+                [
+                    "name" => "Timur Malikin",
+                    "email" => "timur.malikin@gmail.com",
+                    "role" => "Developer"
+                ]
+            ]
+        ];
 
         $this->assertEquals($arr, jsonDecode($file));
         $this->assertEquals($file, jsonEncode($arr));
